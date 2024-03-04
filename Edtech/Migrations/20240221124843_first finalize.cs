79f0541cacf90f143a378459fa8c35a4e93712ba
prathamesh207 <prathameshlad207@gmail.com>
2024-03-05 01:57:07 +0530

    update

---
+++ Edtech/Migrations/20240221124843_first finalize.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Edtech.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstfinalize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
